36432367284d6e18c17d0a1f176e263568e3a9fc
Andy Gray <andy@whatsthescore.co.uk>
2019-01-23 14:00:45 +0000

    canvas generic

--- svg_tests/canvg/test.js
+++ svg_tests/canvg/test.js
@@ -9,19 +9,31 @@ const fs = require('fs');
 
 void async function () {
 
-    const base = await loadImage('./svgs/base-1.svg');
-    const body = await loadImage('./svgs/body-1.svg');
-    const roof = await loadImage('./svgs/roof-1.svg');
+    const canvasHeight = 1000;
+    const canvasWidth = 240;
 
-    const canvas = createCanvas(240, 1000);
+
+    const bases = [{height:231, anchor:92}];
+    const bodies = [{height:416, anchor:277}];
+    const roofs = [{height:185, anchor:-1}];
+
+    const base0 = await loadImage('./svgs/base-1.svg');
+    const body0 = await loadImage('./svgs/body-1.svg');
+    const roof0 = await loadImage('./svgs/roof-1.svg');
+
+    const baseSvgs = [base0];
+    const bodySvgs = [body0];
+    const roofSvgs = [roof0];
+
+    const canvas = createCanvas(canvasWidth, canvasHeight);
 
     const ctx = canvas.getContext('2d');
 
-    ctx.drawImage(base, 0, 300);
+    ctx.drawImage(baseSvgs[0], 0, canvasHeight - bases[0].height);
 
-    ctx.drawImage(body, 0, 300);
+    ctx.drawImage(bodySvgs[0], 0, canvasHeight - bases[0].anchor - bodies[0].height);
 
-    ctx.drawImage(roof, 0, 300);
+    ctx.drawImage(roofSvgs[0], 0, canvasHeight - bases[0].anchor - bodies[0].anchor - roofs[0].height);
 
     console.log(canvas.toDataURL());
 
