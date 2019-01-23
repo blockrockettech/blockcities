36132ddd594168d77f00de3de61aaacafed8efd7
James Morgan <james.morgan.e@googlemail.com>
2019-01-23 10:21:41 +0000

    Adding optimzed SVG via SVGO

---
+++ svg_tests/svgo_test_1.js
@@ -0,0 +1,68 @@
+const SVGO = require('svgo');
+const PATH = require('path');
+const fs = require('fs');
+
+const svgo = new SVGO({
+    plugins: [
+        {cleanupAttrs: true,},
+        {removeDoctype: true,},
+        {removeXMLProcInst: true,},
+        {removeComments: true,},
+        {removeMetadata: true,},
+        {removeTitle: true,},
+        {removeDesc: true,},
+        {removeUselessDefs: true,},
+        {removeEditorsNSData: true,},
+        {removeEmptyAttrs: true,},
+        {removeHiddenElems: true,},
+        {removeEmptyText: true,},
+        {removeEmptyContainers: true,},
+        {removeViewBox: false,},
+        // {cleanupEnableBackground: true,},
+        // {convertStyleToAttrs: true,},
+        // {convertColors: true,},
+        // {convertPathData: true,},
+        // {convertTransform: true,},
+        {removeUnknownsAndDefaults: true,},
+        {removeNonInheritableGroupAttrs: true,},
+        {removeUselessStrokeAndFill: true,},
+        {removeUnusedNS: true,},
+        {cleanupIDs: true,},
+        {cleanupNumericValues: true,},
+        // {moveElemsAttrsToGroup: true,},
+        // {moveGroupAttrsToElems: true,},
+        // {collapseGroups: true,},
+        // {removeRasterImages: false,},
+        // {mergePaths: true,},
+        // {convertShapeToPath: true,},
+        // {sortAttrs: true,},
+        // {removeDimensions: true,},
+        // {removeAttrs: {attrs: '(stroke|fill)'}}
+    ]
+});
+
+const file_name = 'base-1';
+
+const svgRawFile = fs.readFileSync(`./svgs/${file_name}.svg`);
+
+svgo
+    .optimize(svgRawFile, {
+        path: svgRawFile.path
+    })
+    .then(function (result) {
+
+        console.log(result);
+
+        // {
+        //     // optimized SVG data string
+        //     data: '<svg width="10" height="20">test</svg>'
+        //     // additional info such as width/height
+        //     info: {
+        //         width: '10',
+        //         height: '20'
+        //     }
+        // }
+
+        fs.writeFileSync(`./svg_tests/optimized/${file_name}-svgo-clean.svg`, result.data);
+
+    });
