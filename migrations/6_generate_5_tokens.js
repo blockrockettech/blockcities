3dc38893989007d79a983b097ff851f341c9a918
Andy Gray <andy@whatsthescore.co.uk>
2019-02-02 14:47:43 +0000

    cleanup

--- migrations/6_generate_5_tokens.js
+++ migrations/6_generate_5_tokens.js
@@ -6,11 +6,7 @@ const infuraApikey = '8d878f1ce20b4e2fa9eea01668281193';
 module.exports = async function (deployer, network, accounts) {
     const _blockCitiesVendingMachine = await BlockCitiesVendingMachine.deployed();
 
-    console.log(_blockCitiesVendingMachine);
-
     let _owner = accounts[0];
-
-    console.log(_owner);
 
     // Add some credit for each account
     await _blockCitiesVendingMachine.addCredit(_owner, {from: _owner});
--- truffle.js
+++ truffle.js
@@ -38,13 +38,6 @@ module.exports = {
       gas: 6721975,
       gasPrice: 1
     },
-    testrpc: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: '*',
-      gas: 0xfffffffffff,
-      gasPrice: 0x01
-    },
     coverage: {
       host: "localhost",
       network_id: "*",
