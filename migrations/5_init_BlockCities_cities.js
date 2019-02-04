f7da389d0f6c21f49b2c50dea3a5a83d3c016538
James Morgan <james.morgan.e@googlemail.com>
2019-02-04 13:50:29 +0000

    WIP

--- migrations/5_init_BlockCities_cities.js
+++ migrations/5_init_BlockCities_cities.js
@@ -3,7 +3,10 @@ let BlockCities = artifacts.require('./BlockCities.sol');
 module.exports = async function (deployer, network, accounts) {
     const _blockCities = await BlockCities.deployed();
 
+    // Note: these cities must also be inside CityGenerator for them to be chosen/used
+
+    await _blockCities.addCity(web3.utils.fromAscii("San Francisco"), {from: accounts[0]});
     await _blockCities.addCity(web3.utils.fromAscii("Atlanta"), {from: accounts[0]});
     await _blockCities.addCity(web3.utils.fromAscii("Chicago"), {from: accounts[0]});
-
+    await _blockCities.addCity(web3.utils.fromAscii("New York City"), {from: accounts[0]});
 };
