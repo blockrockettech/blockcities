e73c4973e38cc7a21bce2cc5a82885a801395391
Andy Gray <andy@whatsthescore.co.uk>
2019-02-04 09:39:27 +0000

    err

--- truffle.js
+++ truffle.js
@@ -5,53 +5,39 @@ let mnemonic = require('./mnemonic');
 // Check gas prices before live deploy - https://ethgasstation.info/
 
 module.exports = {
-  mocha: {
-    useColors: true,
-    reporter: 'eth-gas-reporter',
-    reporterOptions: {
-      currency: 'USD',
-      gasPrice: 3
-    }
-  },
-  compilers: {
-    solc: {
-      settings: {
-        optimizer: {
-          enabled: true, // Default: false
-          runs: 200      // Default: 200
+    mocha: {
+        useColors: true,
+        // reporter: 'eth-gas-reporter',
+        // reporterOptions: {
+        //   currency: 'USD',
+        //   gasPrice: 3
+        // }
+    },
+    compilers: {
+        solc: {
+            settings: {
+                optimizer: {
+                    enabled: true, // Default: false
+                    runs: 200      // Default: 200
+                },
+            }
+        }
+    },
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 7545,
+            gas: 0xfffffffffff, // <-- Use this high gas value
+            gasPrice: 0x01,    // <-- Use this low gas price
+            network_id: '5777', // Match any network id
         },
-      }
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApikey}`);
+            },
+            network_id: 3,
+            gas: 7000000, // default = 4712388
+            gasPrice: 4000000000 // default = 100 gwei = 100000000000
+        },
     }
-  },
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 7545,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01,
-      network_id: '*', // Match any network id
-    },
-    ganache: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
-      gas: 6721975,
-      gasPrice: 1
-    },
-    coverage: {
-      host: "localhost",
-      network_id: "*",
-      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-      gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01      // <-- Use this low gas price
-    },
-    ropsten: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApikey}`);
-      },
-      network_id: 3,
-      gas: 7000000, // default = 4712388
-      gasPrice: 4000000000 // default = 100 gwei = 100000000000
-    },
-  }
 };
