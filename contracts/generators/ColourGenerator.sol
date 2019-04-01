76da734c5e89e09dd1f2290cb6ec1df8f9a9dc2d
Andy Gray <andy@whatsthescore.co.uk>
2019-04-01 15:59:21 +0100

    tweaking

--- contracts/generators/ColourGenerator.sol
+++ contracts/generators/ColourGenerator.sol
@@ -8,6 +8,9 @@ contract ColourGenerator is Ownable {
 
     event Colours(uint256 exteriorColorway, uint256 backgroundColorway);
 
+    uint256 public exteriors = 20;
+    uint256 public backgrounds = 8;
+
     function generate(address _sender)
     external
     returns (
@@ -16,8 +19,8 @@ contract ColourGenerator is Ownable {
     ) {
         bytes32 hash = blockhash(block.number);
 
-        uint256 exteriorColorwayRandom = generate(hash, _sender, 20);
-        uint256 backgroundColorwayRandom = generate(hash, _sender, 8);
+        uint256 exteriorColorwayRandom = generate(hash, _sender, exteriors);
+        uint256 backgroundColorwayRandom = generate(hash, _sender, backgrounds);
 
         emit Colours(exteriorColorwayRandom, backgroundColorwayRandom);
 
@@ -28,5 +31,14 @@ contract ColourGenerator is Ownable {
         randNonce++;
         bytes memory packed = abi.encodePacked(_hash, _sender, randNonce);
         return uint256(keccak256(packed)) % _max;
+    }
+
+    function updateExteriors(uint256 _exteriors) public onlyOwner {
+        exteriors = _exteriors;
+    }
+
+
+    function updateBackgrounds(uint256 _backgrounds) public onlyOwner {
+        backgrounds = _backgrounds;
     }
 }
